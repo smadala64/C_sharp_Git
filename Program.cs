4b348929480b9ba6a4d4a62a49561f8de778e928
Sridhar Madala <112364503+smadala64@users.noreply.github.com>
2023-05-03 14:17:34 -0400

    Sample C# code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Car
+{
+string color = "Red";
+
+static void Main(string[] args)
+{
+    Car MyObj = new Car();
+    Console.WriteLine("The color of car is : "+ MyObj.color);
+}
+}
